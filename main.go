4dd75f03669b51c33ed35f2f8867e88bac36442b
ystays <85421593+ystays@users.noreply.github.com>
2025-08-23 15:33:59 +0800

    Removed second server, added query parameters

--- main.go
+++ main.go
@@ -14,7 +14,15 @@ const keyServerAddr = "serverAddr"
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s\n",
+		ctx.Value(keyServerAddr),
+		hasFirst, first,
+		hasSecond, second)
 	io.WriteString(w, "This is herewego, a Go server!\n")
 }
 
@@ -32,6 +40,7 @@ func main() {
 	mux.HandleFunc("/hello", getHello)
 
 	ctx, cancelCtx := context.WithCancel(context.Background())
+
 	serverOne := &http.Server{
 		Addr:    ":3333",
 		Handler: mux,
@@ -41,34 +50,13 @@ func main() {
 		},
 	}
 
-	serverTwo := &http.Server{
-		Addr:    ":4444",
-		Handler: mux,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-			return ctx
-		},
+	err := serverOne.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server one closed\n")
+	} else if err != nil {
+		fmt.Printf("error listening for server one: %s\n", err)
 	}
-
-	go func() {
-		err := serverOne.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server one closed\n")
-		} else if err != nil {
-			fmt.Printf("error listening for server one: %s\n", err)
-		}
-		cancelCtx()
-	}()
-
-	go func() {
-		err := serverTwo.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server two closed\n")
-		} else if err != nil {
-			fmt.Printf("error listening for server two: %s\n", err)
-		}
-		cancelCtx()
-	}()
+	cancelCtx()
 
 	<-ctx.Done()
 
