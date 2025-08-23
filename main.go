f296ff81528ce3eacd0a1882b47a0f6eb89f8cad
ystays <85421593+ystays@users.noreply.github.com>
2025-08-23 18:50:35 +0800

    Added request body and header

--- main.go
+++ main.go
@@ -19,10 +19,16 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	hasSecond := r.URL.Query().Has("second")
 	second := r.URL.Query().Get("second")
 
-	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s\n",
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("could not read body: %s\n", err)
+	}
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s, body: \n%s\n",
 		ctx.Value(keyServerAddr),
 		hasFirst, first,
-		hasSecond, second)
+		hasSecond, second,
+		body)
 	io.WriteString(w, "This is herewego, a Go server!\n")
 }
 
@@ -30,7 +36,13 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
-	io.WriteString(w, "Hello, HTTP!\n")
+	myName := r.PostFormValue("name")
+	if myName == "" {
+		w.Header().Set("x-missing-field", "name")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	io.WriteString(w, fmt.Sprintf("Hello, %s!\n", myName))
 }
 
 func main() {
